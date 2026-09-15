beb0499eaed32359bd8be57417cbf9dadeca8acd
janic <469342045@qq.com>
2026-09-15 09:48:47 +0800

    使用raga进行评估

--- 01-baseline_rag/model.py
+++ 01-baseline_rag/model.py
@@ -22,10 +22,10 @@ class RagLLM(object):
     def __init__(self):
         super().__init__()
         self.client = OpenAI(base_url="http://localhost:11434/v1/",
-                             api_key="qwen3")
+                             api_key="qwen3:14b")
 
     def __call__(self, prompt: str, **kwargs):
-        completion = self.client.completions.creat(model="qwen3.5:4b",
+        completion = self.client.completions.create(model="qwen3:14b",
                                                    prompt=prompt,
                                                    temperature=kwargs.get("temperature", 0.1),
                                                    top_p=kwargs.get("top_p", 0.9),
@@ -42,14 +42,14 @@ class QwenLLM(LLM):
     def __init__(self):
         super().__init__()
         self.client = OpenAI(base_url="http://localhost:11434/v1/",
-                             api_key="qwen3")
+                             api_key="qwen3:14b")
 
     def _call(self,
               prompt: str,
               stop: Optional[List[str]] = None,
               run_prompt: Optional[CallbackManagerForLLMRun] = None,
               **kwargs: Any):
-        completion = self.client.completions.create(model="qwen3.5:4b",
+        completion = self.client.completions.create(model="qwen3:14b",
                                                     prompt=prompt,
                                                     temperature=kwargs.get("temperature", 0.1),
                                                     top_p=kwargs.get("top_p", 0.9),
@@ -57,16 +57,35 @@ class QwenLLM(LLM):
                                                     stream=kwargs.get("stream", False))
         return completion.choices[0].text
 
+    # def _call(self,
+    #           prompt: str,
+    #           stop: Optional[List[str]] = None,
+    #           run_manager: Optional[CallbackManagerForLLMRun] = None,
+    #           **kwargs: Any):
+    #     completion = self.client.chat.completions.create(
+    #         model="qwen3:14b",
+    #         messages=[
+    #             {"role": "system", "content": "You are a helpful assistant that only outputs valid JSON."},
+    #             {"role": "user", "content": prompt}
+    #         ],
+    #         temperature=kwargs.get("temperature", 0.1),
+    #         top_p=kwargs.get("top_p", 0.9),
+    #         max_tokens=kwargs.get("max_tokens", 4096),
+    #         response_format={"type": "json_object"},  # 关键：强制 JSON 输出
+    #         stream=kwargs.get("stream", False)
+    #     )
+    #     return completion.choices[0].message.content
+
     @property
     def _llm_type(self)->str:
-        return "rag_llm_qwen3.5_4b"
+        return "rag_llm_qwen3:14b"
 
 
 class RagEmbedding(object):
     def __init__(self, model_path=r"D:\code_project\models\bge-m3",
                  device="auto"):
         self.embedding = HuggingFaceEmbeddings(model_name=model_path,
-                                               model_kwargs={"device": "cpu"})
+                                               model_kwargs={"device": "cuda"})
 
     def get_embedding_fun(self):
         return self.embedding
